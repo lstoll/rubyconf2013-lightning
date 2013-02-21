b8957915defb18209d2978a9136f86a37e9364c1
Lincoln Stoll <lstoll@me.com>
2013-02-21 14:56:42 +1100

    run pry if app.rb launched directly.

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'pg'
 require 'sequel'
+require 'pry'
 
 Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 get '/' do
   'Hello world!'
+end
+
+# REPL me up if we're launched directly
+if __FILE__ == $0
+  binding.pry
+  exit
 end
